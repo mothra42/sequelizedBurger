8e6ad0dbbd54f972701ed7f7b95681b0e58d8adb
Adam Farmer <adam@Adams-MacBook-Air.local>
2017-08-29 14:20:18 -0700

    added sequelize stuff

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name)
+VALUES("Big Kahuna Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES("Bug Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES("Vegan Burger?");
